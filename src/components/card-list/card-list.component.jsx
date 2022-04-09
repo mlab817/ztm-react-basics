e493bb51db4e39a18185cd2f09aeed2784e2942b
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-04-09 11:45:33 +0800

    don refactoring to functional comps

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,20 +3,14 @@ import { Component } from "react"
 import './card-list.styles.css'
 import Card from "./card.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props
-
-    return (
-      <div className="card-list">
-        {
-          monsters.map(monster => {
-            return <Card monster={monster}/>
-          })
-        }
-      </div>
-    )
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {
+      monsters.map(monster => {
+        return <Card key={monster.id} monster={monster}/>
+      })
+    }
+  </div>
+)
 
 export default CardList
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,16 +2,12 @@ import { Component } from "react"
 
 import './search-box.styles.css'
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        type="search"
-        className={`search-box ${this.props.className}`}
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}/>
-    )
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler}) => (
+  <input
+    type="search"
+    className={`search-box ${className}`}
+    placeholder={placeholder}
+    onChange={onChangeHandler}/>
+)
 
 export default SearchBox
